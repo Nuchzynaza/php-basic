85aca241e7c0281141dafb20e9f0d2d8057323b9
nuchzynaza <nuchnuchjung@gmail.com>
2022-07-03 16:11:25 +0700

    first commit

---
+++ check.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if($username == 'admin' && $password == '1234'){
+        echo "Welcome to Dashboard";
+        $_SESSION['username'] =$username;
+        echo "<br/>";
+        echo "Hello...." .$_SESSION['username'];      
+        header ("location: dashboard.php"); 
+
+   }else{
+        echo "Username or Password ผิดพลาด";
+   }
+
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/bootstrap.css" />
+    <script src="./js/popper.js"> </script>
+    <script src="./js/bootstrap.js"> </script>
+
+</head>
+
+<body>
+    <?php
+        include "./navbar.php" ;
+
+        echo "ยินดีต้อนรับเข้าสู่เว็บไซด์";
+        echo "<h1> Welcome to website </h1>";
+        
+        
+        $year=2565;
+        $crhish =$year-543;
+        echo "$year";    
+        echo "ปี พ.ศ. $year ค.ศ. $crhish <br/>";
+        echo "ปี พ.ศ.". $year ;  
+        $salary = 25000;
+        echo "เงินเดือน $salary บาท <br/>";
+        echo "เงินเดือน". $salary . "บาท";
+        echo $year;
+        
+        $name= "Nuchzy";
+        $age = 25;
+        echo "<br/> สวัสดี " .$name ;
+        define("WEB", "https://sanook.com/");
+        echo "<br/> Link to : ".WEB;
+    
+
+    ?>
+
+</body>
+
+</html>
